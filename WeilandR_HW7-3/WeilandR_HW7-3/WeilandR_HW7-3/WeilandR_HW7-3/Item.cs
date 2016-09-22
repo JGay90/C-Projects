22234fe99089902a4b213f4ed6fe4a9c91d54963
gloverx4621 <gloverx4621@ATC15604>
2016-09-22 11:22:35 -0400

    Added effect's for items

--- WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/Item.cs
+++ WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/Item.cs
@@ -11,12 +11,52 @@ namespace WeilandR_HW7_3
         public string Name { get; set; }
         public int Value { get; set; }
         public string Effect { get; set; }
-
+        public int durability { get; set; }
         public Item()
         {
             Name = "You didn't give me anything";
             Value = 0;
             Effect = "None";
+            durability = 1;
+        }
+        public string UseEffect(Player player)
+        {
+            string i;
+            switch (Effect)
+            {
+                case "Heal1":
+                    Heal(player);
+                    i = "you recover 5 HP";
+                    break;
+
+                case "Def1":
+                    Def(player);
+                    i = "your defense goes up by 2";
+                    break;
+
+                case "Atk1":
+                    Atk(player);
+                    i = "your attack goes up by 2";
+                    break;
+
+                default:
+                    //Add box/
+                    break;
+            }
+            return i;
+        }
+        public void Heal(Player playerHeal)
+        {
+            playerHeal.HP = playerHeal.HP + 5;
+
+        }
+        public void Def(Player playerDef)
+        {
+            playerDef.AC = playerDef.AC + 2;
+        }
+        public void Atk(Player playerAtk)
+        {
+            playerAtk.ATK = playerAtk.ATK + 2;
         }
     }
 }

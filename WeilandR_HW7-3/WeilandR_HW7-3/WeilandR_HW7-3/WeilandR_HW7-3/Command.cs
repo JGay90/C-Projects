0b459b30ba11d9fb4338a85b6d0784ce2ea1f3da
gloverx4621 <gloverx4621@ATC15604>
2016-10-27 10:46:28 -0400

    Messed with the coomands

--- WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/Command.cs
+++ WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/Command.cs
@@ -59,15 +59,14 @@ namespace WeilandR_HW7_3
         public string Go()
         {
             string go = "";
-
-
+            
 
             return go;
         }
         public string Look()
         {
             string look = "";
-
+            
 
 
             return look;

fbc8d73582032dbfacaf9cbda3c0b9249f46c650
Jeff Nelson <nelsonj5726@student.faytechcc.edu>
2016-10-30 18:21:22 -0400

    Added a list of rooms. Some of you may recognize this.

--- WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/Command.cs
+++ WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/Command.cs
@@ -99,49 +99,10 @@ namespace WeilandR_HW7_3
         {
             string go = "";
 
+            roomList(direction, world.worldList[player.Location].RID);
 
-            if (world.worldList[player.Location].mobList.Count == 0)
-            {
-                if (direction == "ahead")
-                {
-                    if (this.world.worldList[player.Location].RID == 9)
-                    {
-                        MessageBox.Show("You are at the end of the " +
-                          "dungeon. You can go no further.");
-                    }
-                    else
-                    {
-                        player.Location++;
-                         go = Look();
-                    }
-                }
-                else if (direction == "back")
-                {
-                    if (this.world.worldList[player.Location].RID == 1)
-                    {
-                        MessageBox.Show("You are at the beginning of the " +
-                            "dungeon. You can go no further.");
-                    }
-                    else
-                    {
-                        player.Location--;
-                        go = Look();
-                    }
-                    
-                }
-                else
-                {
-                    go = "\nThat is not a valid direction.";
-                }
 
-            }
-            else
-            {
-                go = "\nThe monster blocks your path.";
-            }
-            
-
-                return go;
+            return go;
         }
         public string Look()
         {
@@ -296,6 +257,461 @@ namespace WeilandR_HW7_3
                           "Your score was: " + Score() + "\n" +
                           "Have a Nice Day";
             return quit;
+        }
+
+        public void roomList(string dir, int roomID)
+        {
+            if (dir == "North")
+            {
+                switch (roomID)
+                {
+                    case 0:
+                        player.Location = 2;
+                        break;
+                    case 2:
+                        player.Location = 7;
+                        break;
+                    case 8:
+                        player.Location = 3;
+                        break;
+                    case 9:
+                        player.Location = 10;
+                        break;
+                    case 10:
+                        player.Location = 11;
+                        break;
+                    case 15:
+                        player.Location = 17;
+                        break;
+                    case 17:
+                        player.Location = 20;
+                        break;
+                    case 19:
+                        player.Location = 18;
+                        break;
+                    case 24:
+                        player.Location = 25;
+                        break;
+                    case 29:
+                        player.Location = 26;
+                        break;
+                    case 32:
+                        player.Location = 33;
+                        break;
+                    case 36:
+                        player.Location = 37;
+                        break;
+                    case 38:
+                        player.Location = 39;
+                        break;
+                    case 41:
+                        player.Location = 32;
+                        break;
+                    case 42:
+                        player.Location = 41;
+                        break;
+                    case 45:
+                        player.Location = 43;
+                        break;
+                    case 46:
+                        player.Location = 44;
+                        break;
+                    case 47:
+                        player.Location = 46;
+                        break;
+                    case 52:
+                        player.Location = 50;
+                        break;
+                    case 55:
+                        player.Location = 54;
+                        break;
+                }
+            }
+            else if (dir == "East")
+            {
+                switch (roomID)
+                {
+                    case 0:
+                        player.Location = 3;
+                        break;
+                    case 1:
+                        player.Location = 0;
+                        break;
+                    case 2:
+                        player.Location = 4;
+                        break;
+                    case 4:
+                        player.Location = 5;
+                        break;
+                    case 6:
+                        player.Location = 1;
+                        break;
+                    case 7:
+                        player.Location = 9;
+                        break;
+                    case 10:
+                        player.Location = 31;
+                        break;
+                    case 11:
+                        player.Location = 12;
+                        break;
+                    case 12:
+                        player.Location = 13;
+                        break;
+                    case 13:
+                        player.Location = 15;
+                        break;
+                    case 14:
+                        player.Location = 17;
+                        break;
+                    case 17:
+                        player.Location = 16;
+                        break;
+                    case 16:
+                        player.Location = 18;
+                        break;
+                    case 19:
+                        player.Location = 20;
+                        break;
+                    case 23:
+                        player.Location = 10;
+                        break;
+                    case 24:
+                        player.Location = 23;
+                        break;
+                    case 25:
+                        player.Location = 26;
+                        break;
+                    case 27:
+                        player.Location = 24;
+                        break;
+                    case 28:
+                        player.Location = 30;
+                        break;
+                    case 31:
+                        player.Location = 32;
+                        break;
+                    case 37:
+                        player.Location = 38;
+                        break;
+                    case 40:
+                        player.Location = 39;
+                        break;
+                    case 42:
+                        player.Location = 43;
+                        break;
+                    case 43:
+                        player.Location = 44;
+                        break;
+                    case 48:
+                        player.Location = 49;
+                        break;
+                    case 51:
+                        player.Location = 50;
+                        break;
+                    case 53:
+                        player.Location = 48;
+                        break;
+                    case 54:
+                        player.Location = 53;
+                        break;
+
+                }
+            }
+            else if (dir == "West")
+            {
+                switch (roomID)
+                {
+                    case 0:
+                        player.Location = 1;
+                        break;
+                    case 1:
+                        player.Location = 6;
+                        break;
+                    case 3:
+                        player.Location = 0;
+                        break;
+                    case 4:
+                        player.Location = 2;
+                        break;
+                    case 5:
+                        player.Location = 4;
+                        break;
+                    case 9:
+                        player.Location = 7;
+                        break;
+                    case 10:
+                        player.Location = 23;
+                        break;
+                    case 12:
+                        player.Location = 11;
+                        break;
+                    case 13:
+                        player.Location = 12;
+                        break;
+                    case 15:
+                        player.Location = 13;
+                        break;
+                    case 16:
+                        player.Location = 17;
+                        break;
+                    case 17:
+                        player.Location = 14;
+                        break;
+                    case 18:
+                        player.Location = 16;
+                        break;
+                    case 20:
+                        player.Location = 11;
+                        break;
+                    case 23:
+                        player.Location = 24;
+                        break;
+                    case 24:
+                        player.Location = 27;
+                        break;
+                    case 25:
+                        player.Location = 27;
+                        break;
+                    case 26:
+                        player.Location = 25;
+                        break;
+                    case 27:
+                        player.Location = 55;
+                        break;
+                    case 30:
+                        player.Location = 28;
+                        break;
+                    case 31:
+                        player.Location = 10;
+                        break;
+                    case 32:
+                        player.Location = 31;
+                        break;
+                    case 38:
+                        player.Location = 37;
+                        break;
+                    case 39:
+                        player.Location = 40;
+                        break;
+                    case 40:
+                        player.Location = 36;
+                        break;
+                    case 41:
+                        player.Location = 54;
+                        break;
+                    case 43:
+                        player.Location = 42;
+                        break;
+                    case 44:
+                        player.Location = 43;
+                        break;
+                    case 48:
+                        player.Location = 53;
+                        break;
+                    case 49:
+                        player.Location = 48;
+                        break;
+                    case 50:
+                        player.Location = 51;
+                        break;
+                    case 53:
+                        player.Location = 54;
+                        break;
+                }
+            }
+            else if (dir == "South")
+            {
+                switch (roomID)
+                {
+                    case 2:
+                        player.Location = 0;
+                        break;
+                    case 3:
+                        player.Location = 8;
+                        break;
+                    case 7:
+                        player.Location = 2;
+                        break;
+                    case 10:
+                        player.Location = 9;
+                        break;
+                    case 11:
+                        player.Location = 10;
+                        break;
+                    case 18:
+                        player.Location = 19;
+                        break;
+                    case 20:
+                        player.Location = 17;
+                        break;
+                    case 25:
+                        player.Location = 24;
+                        break;
+                    case 26:
+                        player.Location = 29;
+                        break;
+                    case 32:
+                        player.Location = 41;
+                        break;
+                    case 33:
+                        player.Location = 32;
+                        break;
+                    case 37:
+                        player.Location = 36;
+                        break;
+                    case 39:
+                        player.Location = 38;
+                        break;
+                    case 41:
+                        player.Location = 42;
+                        break;
+                    case 43:
+                        player.Location = 45;
+                        break;
+                    case 44:
+                        player.Location = 46;
+                        break;
+                    case 46:
+                        player.Location = 47;
+                        break;
+                    case 50:
+                        player.Location = 52;
+                        break;
+                    case 54:
+                        player.Location = 55;
+                        break;
+                    case 55:
+                        player.Location = 27;
+                        break;
+                }
+            }
+            else if (dir == "NorthWest")
+            {
+                switch (roomID)
+                {
+                    case 17:
+                        player.Location = 17;
+                        break;
+                    case 25:
+                        player.Location = 28;
+                        break;
+                    case 33:
+                        player.Location = 35;
+                        break;
+                    case 38:
+                        player.Location = 40;
+                        break;
+                    case 49:
+                        player.Location = 47;
+                        break;
+                    case 50:
+                        player.Location = 48;
+                        break;
+                }
+            }
+
+            else if (dir == "NorthEast")
+            {
+                switch (roomID)
+                {
+                    case 1:
+                        player.Location = 2;
+                        break;
+                    case 2:
+                        player.Location = 9;
+                        break;
+                    case 12:
+                        player.Location = 14;
+                        break;
+                    case 13:
+                        player.Location = 27;
+                        break;
+                    case 21:
+                        player.Location = 19;
+                        break;
+                    case 35:
+                        player.Location = 36;
+                        break;
+                    case 33:
+                        player.Location = 34;
+                        break;
+                    case 37:
+                        player.Location = 39;
+                        break;
+                    case 48:
+                        player.Location = 47;
+                        break;
+                    case 50:
+                        player.Location = 49;
+                        break;
+                }
+            }
+
+            else if (dir == "SouthWest")
+            {
+                switch (roomID)
+                {
+                    case 2:
+                        player.Location = 1;
+                        break;
+                    case 9:
+                        player.Location = 2;
+                        break;
+                    case 14:
+                        player.Location = 12;
+                        break;
+                    case 19:
+                        player.Location = 21;
+                        break;
+                    case 30:
+                        player.Location = 25;
+                        break;
+                    case 36:
+                        player.Location = 35;
+                        break;
+                    case 39:
+                        player.Location = 37;
+                        break;
+                    case 34:
+                        player.Location = 33;
+                        break;
+                    case 47:
+                        player.Location = 48;
+                        break;
+                    case 49:
+                        player.Location = 50;
+                        break;
+
+                }
+            }
+
+            else if (dir == "SouthEast")
+            {
+                switch (roomID)
+                {
+                    case 26:
+                        player.Location = 25;
+                        break;
+                    case 35:
+                        player.Location = 33;
+                        break;
+                    case 36:
+                        player.Location = 34;
+                        break;
+                    case 40:
+                        player.Location = 38;
+                        break;
+                    case 47:
+                        player.Location = 49;
+                        break;
+                    case 48:
+                        player.Location = 50;
+                        break;
+                }
+            }
+
+            Look();
         }
     }
 }

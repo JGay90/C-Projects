1c7a43dacd3998a03620e35ebca422602ea1380c
weilandr0843 <weilandr0843@ATC15601>
2016-11-01 11:37:28 -0400

    Made you able to press the Enter key to submit.

--- WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/Form1.Designer.cs
+++ WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/WeilandR_HW7-3/Form1.Designer.cs
@@ -487,6 +487,7 @@
             // 
             // dungeonCrawlForm
             // 
+            this.AcceptButton = this.submitButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1045, 477);
